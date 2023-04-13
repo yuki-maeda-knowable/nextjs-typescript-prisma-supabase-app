8e801a6bf910169cd7738f1db152c5c4cc105cab
yuki-maeda-knowable <yuki_maeda@knowable.jp>
2023-04-13 10:38:22 +0900

    タグのコンポーネント作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "tailwindcss/tailwind.css";
 import { ThemeProvider } from "@mui/material";
 import "/styles/globals.css";
 import theme from "../theme";
+import "/components/TagInput.css";
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
