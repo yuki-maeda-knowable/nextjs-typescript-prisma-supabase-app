7bfdb153e66f34bc2acf01ece9e5cf90bc300dc0
yuki-maeda-knowable <yuki_maeda@knowable.jp>
2023-05-01 12:40:31 +0900

    テーマをlightモードに変更

--- theme.js
+++ theme.js
@@ -1,14 +1,8 @@
-import { lime, orange } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    mode: "dark",
-    primary: orange,
-    secondary: lime,
-    text: {
-      primary: "#FFFFFF",
-    },
+    mode: "light",
   },
 });
 
