9cad4dccaaf112ef6558c414c063205e61286749
yuki-maeda-knowable <yuki_maeda@knowable.jp>
2023-05-30 19:11:48 +0900

    みずほ銀行のサイトを試しに

--- pages/api/scraping/knowable.tsx
+++ pages/api/scraping/knowable.tsx
@@ -12,7 +12,8 @@ export default async function handler(req, res) {
     // headless: true,
   };
 
-  const url = "https://scraping-training.vercel.app";
+  // const url = "https://scraping-training.vercel.app";
+  const url = "https://www.mizuhobank.co.jp/rate_fee/rate_interest.html";
   // const url = "https://knowable.co.jp/";
 
   // const inputJobHighFields = [
@@ -48,13 +49,14 @@ export default async function handler(req, res) {
     // "#hmessage .inner .hmessage__textarea .hmessage__text"がよみこまれるまで待機
     await page.waitForSelector(
       // "#hmessage .inner .hmessage__textarea .hmessage__text"
-      "header"
+      "#mainCol"
     );
 
     // const jobDetails = await scrapeJobDetails(page, browser);
     //  "#hmessage .inner .hmessage__textarea .hmessage__text"のテキストを取得しresultに格納
     const result = await page.$eval(
-      "header div a span.text-sm",
+      "#mainCol .h1Tit",
+      // "header div a span.text-sm",
       (element) => element.textContent
     );
 
